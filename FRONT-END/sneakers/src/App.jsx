74b7e7843699aac7088cc540e1bdbd5d5af49252
Nanditha <bknanditha.2020@gmail.com>
2024-04-23 12:44:21 +0530

    updated login and logout ednpoints to use cookies

--- FRONT-END/sneakers/src/App.jsx
+++ FRONT-END/sneakers/src/App.jsx
@@ -1,22 +1,24 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import React from 'react'
-import Data from './Components/Data.jsx'
 import Landingpage from './Components/Landingpage.jsx'
 import Home from "./Home.jsx"
 import Form from  './Components/signup.jsx';
 import Users from  './Crud.jsx';
 import CreateUser from  './CreateUser.jsx';
+import Log from './Components/login.jsx';
+import UpdateUser from './UserUpdate.jsx';
 
 function App() {
   return (
     <Router>
     <Routes>
       <Route path="/" element={<Landingpage />} />
-      <Route path="/signup" element={<Form />} />
-      <Route path="/signup" element={<Form />} />
+      <Route path="/signup" element={<CreateUser />} />
       <Route path="/Home" element={<Home />} />
       <Route path="/Users" element={<Users />} />
       <Route path="/create" element={<CreateUser />} />
+      <Route path="/login" element={<Log />} />
+      <Route path="/update/:id" element={<UpdateUser />} />
 
     </Routes>
   </Router>
